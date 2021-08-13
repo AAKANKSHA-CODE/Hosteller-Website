18cd552d5a79eee06059e9c512aee4c407d71131
aakanksha-code <aakanksharaj05@gmail.com>
2021-08-14 02:36:11 +0530

    hosteller

---
+++ about1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <style>
+        header {
+        
+            background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),url("images/ab.jpg");
+        }
+    </style>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/about.css">
+
+    <!-- <link rel="stylesheet" href="../css/home.css"> -->
+
+</head>
+
+<body>
+    <header>
+        <div class="header_menu">
+            <div class="logo">
+            
+                <img src="images/logo.jpg" alt="logo">
+            </div>
+            <div class="menu_list">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about1.php">About</a></li>
+                    <li><a href="gallery1.php">Gallery</a></li>
+                    <li><a href="registration1.php">Registration</a></li>
+                    <li><a href="details1.php">Details</a></li>
+                    <li><a href="contact1.php">contact us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="content">
+    
+        
+    <h1>About Us</h1>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda dolorem sed, saepe ad atque cum facere consequuntur, quas aliquid quidem rerum? Dolore, nisi assumenda nostrum excepturi eaque minima adipisci quidem?
+    <br>
+    <br>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, nostrum necessitatibus qui autem natus libero dolor reiciendis, quibusdam deserunt at, in id aliquid dolore? Nam, id ad? Esse, et perferendis?
+    <br>
+    <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, possimus, sit labore, culpa perferendis corporis tempora quis dolorem eveniet repellendus eum. Nulla ut quidem harum ipsum unde, natus facilis labore?   <br>
+    <br>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit explicabo dolorum aliquid voluptates! Iste aperiam libero nam tempore dignissimos quisquam veniam vel cupiditate enim molestias facere, quas tenetur minus nobis.<br>
+    </p>
+</div>
+    </header>
+</body>
+
+</html>
